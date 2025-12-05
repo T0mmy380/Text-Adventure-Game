bb74c073981c41d04bbe7515adc27c189c657705
Tommy Lev <tommy.levasseur@icloud.com>
2025-12-05 03:46:55 +0000

    Added 3 Types

---
+++ Src/FireType.py
@@ -0,0 +1,9 @@
+class FireType:
+
+    def __init__(self, Type):
+        self.id = 3
+        self.name = "Fire"
+        self.colour = "#F08030"
+        self.description = "Fire type Pokémon are known for their fiery nature and abilities related to heat and flames."
+        self.typeBound = 3
+        self.classBound = 3
---
+++ Src/WaterType.py
@@ -0,0 +1,9 @@
+class WaterType:
+
+    def __init__(self, Type):
+        self.id = 1
+        self.name = "Water"
+        self.colour = "#6890F0"
+        self.description = "Water type Pokémon are known for their affinity with aquatic environments. They often have abilities related to water manipulation and swimming."
+        self.typeBound = 1
+        self.classBound = 1
