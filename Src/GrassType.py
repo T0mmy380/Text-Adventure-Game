0935f56e093ad3d8845982c7f0e700fa0f8cacb2
Tommy Lev <tommy.levasseur@icloud.com>
2025-12-05 13:59:55 +0000

    Moves Types to Type folder

--- Src/GrassType.py
+++
@@ -1,9 +0,0 @@
-class GrassType:
-
-    def __init__(self, Type):
-        self.id = 0
-        self.name = "Grass"
-        self.colour = "#78C850"
-        self.description = "Grass type Pokémon are known for their connection to nature and plants. They often have abilities related to growth and healing."
-        self.typeBound = 0
-        self.classBound = 0
